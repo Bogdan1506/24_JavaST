16068dc430799277c16bae11c3f313d02721114a
Bogdan <avdeev1506@mail.ru>
2020-01-13 23:51:20 +0300

    remove

--- task07individual/src/main/java/by/avdeev/task08/controller/command/Command.java
+++
@@ -1,7 +0,0 @@
-package by.avdeev.task08.controller.command;
-
-import by.avdeev.task08.service.exception.ServiceException;
-
-public interface Command {
-    void execute() throws ServiceException;
-}
