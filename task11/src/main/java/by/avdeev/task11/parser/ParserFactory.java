d95b9aaee2153774f83ea0bfc42074238b1d8d02
Bogdan <avdeev1506@mail.ru>
2020-02-12 19:04:29 +0300

    fixed

--- task11/src/main/java/by/avdeev/task11/parser/ParserFactory.java
+++
@@ -1,6 +0,0 @@
-package by.avdeev.task11.parser;
-
-public class ParserFactory {
-    private static final ParserFactory factory = new ParserFactory();
-    private final ParagraphParser paragraphParser = new ParagraphParser();  //todo
-}
